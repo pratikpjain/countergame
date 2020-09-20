cb207611d80b9f13417042b626f9e28bafeecf0b
Pratik Jain <63181338+pratikpjain@users.noreply.github.com>
2020-09-20 18:07:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var i = 0;
+function counter() {
+  i++;
+  document.getElementById('cnt').innerHTML = 'Count = ' + i;
+}
+function zero() {
+  i = 0;
+  document.getElementById('cnt').innerHTML = 'Count = ' + i;
+}
